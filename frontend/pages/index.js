8f67c07370a616c4e0a8ff64e6c334b153311207
dastaraiofficial <dastar.aiofficial@gmail.com>
2025-07-10 09:47:48 +0800

    Delete frontend directory

--- frontend/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Selamat Datang ke TAS.DAR</h1>
-      <p>UI ini sedang dibina semula dalam Next.js.</p>
-      <a href="/chat">Mula Berbual</a>
-    </main>
-  );
-}
