460a50449f1bcde6680d83359111bb0d7d62f92e
dastaraiofficial <dastar.aiofficial@gmail.com>
2025-07-10 14:58:32 +0800

    Create chat.js

---
+++ backend/routes/api/chat.js
@@ -0,0 +1,6 @@
+
+const express = require("express");
+const router = express.Router();
+const { chatWithGPT } = require("../../controllers/chatController");
+router.post("/", chatWithGPT);
+module.exports = router;
