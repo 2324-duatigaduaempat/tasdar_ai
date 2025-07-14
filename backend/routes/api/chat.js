4ebb54e8ccdadaed60e4e9e47fa6f7166006a769
dastaraiofficial <dastar.aiofficial@gmail.com>
2025-07-14 11:00:46 +0800

    Delete backend directory

--- backend/routes/api/chat.js
+++
@@ -1,6 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const { chatWithGPT } = require("../../controllers/chatController");
-router.post("/", chatWithGPT);
-module.exports = router;
